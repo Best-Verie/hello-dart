bb9bac802968301ba14946334c57815ecf2541eb
Best-Verie <irabestverie2016@gmail.com>
2022-09-01 09:13:59 +0200

    feat: adds data types

---
+++ d-types.dart
@@ -0,0 +1,15 @@
+main() {
+  /***
+   * int 
+   * double
+   * string
+   * bool
+   * dynamic
+   */
+
+  int amount1 = 100;
+  var amount2 = 200;
+
+  print('the amount1: $amount1 | the amount1: $amount2 \n');
+}
+
