c66e081a79c6fa86a034bf5208c9a08aaea0a694
Best-Verie <irabestverie2016@gmail.com>
2022-09-01 09:46:23 +0200

    feat: operators on the list

---
+++ operators/op.dart
@@ -0,0 +1,15 @@
+// same as any opther lang we know
+void main() {
+  int num = 10 + 22;
+  num = num % 2;
+  print(num);
+  num = num & 5;
+  print(num);
+  // relational == , ! = , > = < ==
+  if (num == 0) {
+    print(' Zero ');
+  }
+  num = 100;
+  num *= 2;
+  print(num);
+}
